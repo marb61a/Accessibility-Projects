d7af067b8934cf794c8c4b72561808b45a3cf6ef
Martin <marb61a@gmail.com>
2021-01-04 19:59:41 +0000

    Added todo-app spec file

---
+++ cypress-axe-project/cypress/integration/todo-app.spec.js
@@ -0,0 +1,27 @@
+/// <reference types="cypress"/>
+
+describe('Todo application', () => {
+    beforeEach(() => {
+        cy.visit('http://todomvc.com/examples/react');
+        cy.injectAxe();
+    });
+
+    it('should log any accessibility failures', () => {
+        // cy.visit('http://todomvc.com/examples/react');
+
+        // Injects the axe-core runtime
+        // cy.injectAxe();
+
+        // Scans a page for any accessibility failures
+        cy.checkA11y();
+    });
+
+    it('should exclude specific elements on the page', () => {
+        cy.checkA11y({ exclude: ['.learn'] });
+    });
+    
+    it('should only test specific element on the page', () => {
+        cy.checkA11y('.learn');
+    });
+    
+});
