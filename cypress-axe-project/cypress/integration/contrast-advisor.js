d23885d24216745ad34294fe0076d0b385e59123
Martin <marb61a@gmail.com>
2021-01-05 12:13:17 +0000

    Added contrast advisor test

---
+++ cypress-axe-project/cypress/integration/contrast-advisor.js
@@ -0,0 +1,19 @@
+/// <reference types="Cypress" />
+/// <reference types="@applitools/eyes-cypress" />
+
+describe('Contrast Advisor Demo', () => {
+    it('should show contrast issues on Applitools dashboard', () => {
+        cy.eyesOpen({
+            appName: 'Contrast Advisor',
+            batchName: 'Contrast Advisor',
+        });
+
+        cy.visit('https://heavyweight.nl/');
+
+        // Generates snapshot
+        cy.eyesCheckWindow();
+        cy.eyesClose();
+    });
+    
+});
+  
